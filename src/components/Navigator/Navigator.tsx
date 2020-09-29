2c344531e5ae1c301201fa03f85f30cf246ce539
Jaejong Heo <39189062+hapsoa@users.noreply.github.com>
2020-09-30 02:14:07 +0900

    making component

---
+++ src/components/Navigator/Navigator.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+class Navigator extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Navigator</div>
+        <Button>hello</Button>
+      </div>
+    );
+  }
+}
+
+export default Navigator;
